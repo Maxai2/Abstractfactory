c7265949e68dc52b251032b60d1d5eacb7dad467
Хамелеон <maxali946@gmail.com>
2018-02-12 21:56:40 +0400

    50%

---
+++ AbstractFactory/Composite/File.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//--------------------------------------------------
+namespace Composite
+{
+    class File : Component
+    {
+        public File(string name) : base(name) 
+        {}
+
+        public override void Add(Component c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Display(int depth)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Remove(Component c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+//--------------------------------------------------
---
+++ AbstractFactory/Composite/directory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//--------------------------------------------------
+namespace Composite
+{
+    class Directory : Component
+    {
+        public Directory(string name) : base(name)
+        {}
+
+        private List<Component> list = new List<Component>();
+
+        public override void Add(Component c) => list.Add(c);
+
+        public override void Remove(Component c) => list.Remove(c); 
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine();
+        }
+    }
+}
+//--------------------------------------------------
